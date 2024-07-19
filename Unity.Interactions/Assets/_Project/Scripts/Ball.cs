4d0de53ab1772008a4e67e1a52a39b98854e588b
Daniel Müller <daniel.daniel@gmx.ch>
2024-07-19 11:37:25 +0200

    refactoring packages

--- Unity.Interactions/Assets/_Project/Scripts/Ball.cs
+++ Unity.Interactions/Assets/_Project/Scripts/Ball.cs
@@ -1,34 +1,5 @@
 using UnityEngine;
 
-public class Ball: MonoBehaviour, IReplayable
+public class Ball: MonoBehaviour
 {
-    public ITrajectory Trajectory { get; set; }
-    
-    private bool _isPlaying;
-    private float _currentReplayTime;
-    
-    public void Play()
-    {
-        _isPlaying = true;
-    }
-
-    public void Pause()
-    {
-        _isPlaying = false;
-    }
-
-    public void JumpTo(float time)
-    {
-        _currentReplayTime = time;
-    }
-    
-    void Update()
-    {
-        if (!_isPlaying)
-            return;
-        
-        _currentReplayTime += Time.deltaTime;
-        transform.position = Trajectory.GetPosition(_currentReplayTime);
-    }
-
 }
