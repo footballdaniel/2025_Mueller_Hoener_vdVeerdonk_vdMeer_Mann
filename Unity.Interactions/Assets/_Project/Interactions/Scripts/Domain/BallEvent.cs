5ca76f509cbbda60e9f09448a0d1808ccb3c8f3c
Daniel Müller <daniel.daniel@gmx.ch>
2025-01-24 12:17:15 +0100

    export ball events in Lab conditions

--- Unity.Interactions/Assets/_Project/Interactions/Scripts/Domain/BallEvent.cs
+++ Unity.Interactions/Assets/_Project/Interactions/Scripts/Domain/BallEvent.cs
@@ -6,9 +6,15 @@ namespace Interactions.Domain
 	[Serializable]
 	public class BallEvent
 	{
+		public string Name { get; }
+		public float Timestamp { get; }
+		public Vector3 Position { get; }
+		
 		public BallEvent(string name, float timestamp, Vector3 position)
 		{
-			
+			Name = name;
+			Timestamp = timestamp;
+			Position = position;
 		}
 	}
 }
