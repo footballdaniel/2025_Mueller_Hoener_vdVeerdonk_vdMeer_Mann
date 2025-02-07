21ce79a5628d2f5e38ebe62e2aad1c2a5c32f1ff
Daniel Müller <daniel.daniel@gmx.ch>
2025-02-07 14:15:27 +0100

    start working on processing data

--- Python.Analysis/src/domain.py
+++ Python.Analysis/src/domain.py
@@ -17,6 +17,19 @@ class Position:
     y: float
     z: float
 
+    def distance_2d(self, other):
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+
+    def magnitude(self):
+        return (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+
+    def interpolate(self, other: "Position", t: float) -> "Position":
+        return Position(
+            x=self.x + (other.x - self.x) * t,
+            y=self.y + (other.y - self.y) * t,
+            z=self.z + (other.z - self.z) * t
+        )
+
 
 class Footedness(Enum):
     RIGHT = 'Right'
@@ -81,6 +94,21 @@ class Trial:
     start: Action
     end: Action
     dominant_foot_side: Footedness
+    has_missing_data: bool = False
+
+    def distance_between_last_touch_and_pass(self):
+        last_touch = next((action for action in reversed(self.actions) if isinstance(action, Touch)), None)
+        pass_event = next((action for action in self.actions if isinstance(action, Pass)), None)
+
+        distance = last_touch.position.distance_2d(pass_event.position)
+        return distance
+
+    def timing_between_last_touch_and_pass(self):
+        last_touch = next((action for action in reversed(self.actions) if isinstance(action, Touch)), None)
+        pass_event = next((action for action in self.actions if isinstance(action, Pass)), None)
+
+        time_difference = self.timestamps[pass_event.time_index] - self.timestamps[last_touch.time_index]
+        return time_difference
 
     def duration(self):
         first_action = self.actions[0]
@@ -90,4 +118,25 @@ class Trial:
         return last_action_time - first_action_time
 
     def number_of_touches(self):
-        return len([action for action in self.actions if isinstance(action, Touch)])
+        return len(self.actions)
+
+    def average_interpersonal_distance(self):
+        if self.condition == Condition.NO_OPPONENT:
+            return 0
+
+        total_distance = 0
+        for i in range(len(self.hip_positions)):
+            total_distance += self.hip_positions[i].distance_2d(self.opponent_hip_positions[i])
+
+        return total_distance / len(self.hip_positions)
+
+    def interpersonal_distance_at_pass_time(self):
+        if self.condition == Condition.NO_OPPONENT:
+            return 0
+
+        pass_event = next((action for action in self.actions if isinstance(action, Pass)), None)
+
+        position_of_player = self.hip_positions[pass_event.time_index]
+        position_of_opponent = self.opponent_hip_positions[pass_event.time_index]
+
+        return position_of_player.distance_2d(position_of_opponent)
--- Python.Analysis/src/manual_annotations.py
+++ Python.Analysis/src/manual_annotations.py
@@ -4,6 +4,7 @@ from typing import List
 
 from src.domain import Position, Footedness, Foot, Pass, Touch, Trial, NoAction, Side, \
     NoPass, Condition
+from src.interpolation import Interpolator
 
 
 def read_positions(json_positions_data: List[dict]) -> List[Position]:
@@ -46,6 +47,7 @@ def process_file(csv_file: str, json_file: str) -> Trial:
     user_hip_positions = read_positions(json_data.get("UserHipPositions"))
     opponent_hip_positions = read_positions(json_data.get("OpponentHipPositions"))
 
+
     trial = Trial(
         path=path,
         condition=condition,
@@ -60,6 +62,23 @@ def process_file(csv_file: str, json_file: str) -> Trial:
         end=NoAction(),
         dominant_foot_side=Footedness.RIGHT if is_dominant_foot_right else Footedness.LEFT
     )
+
+
+    if not Interpolator.try_interpolate_missing_data(user_dominant_foot_positions):
+        trial.has_missing_data = True
+        print(f"{path}: Dominant foot has lots of missing data in trial: {path}")
+
+    if not Interpolator.try_interpolate_missing_data(user_non_dominant_foot_positions):
+        trial.has_missing_data = True
+        print(f"{path}: Non-dominant foot has lots of missing data in trial: {path}")
+
+    if not Interpolator.try_interpolate_missing_data(user_hip_positions):
+        trial.has_missing_data = True
+        print(f"{path}: User hip has lots of missing data in trial: {path}")
+
+    if not Interpolator.try_interpolate_missing_data(opponent_hip_positions,ignore_start=True, ignore_end=True):
+        trial.has_missing_data = True
+        print(f"{path}: Opponent hip has lots of missing data in trial: {path}")
 
     with open(csv_file, "r") as csv_file_content:
         csv_reader = csv.reader(csv_file_content)
