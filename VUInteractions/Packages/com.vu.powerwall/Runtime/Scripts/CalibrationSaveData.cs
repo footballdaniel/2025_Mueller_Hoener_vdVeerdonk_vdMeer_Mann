87e79333833757e12ff1cbeb69fa061e9d668c61
Daniel Müller <daniel.daniel@gmx.ch>
2025-06-06 22:24:25 +0100

    Add powerwall as embedded package

---
+++ VUInteractions/Packages/com.vu.powerwall/Runtime/Scripts/CalibrationSaveData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CalibrationData
+{
+	public CalibrationData(Vector3 motionTrackerPosition, Quaternion motionTrackerRotation)
+	{
+		Offset = motionTrackerPosition;
+		Rotation = motionTrackerRotation;
+	}
+
+	public Vector3 Offset { get; set; }
+	public Quaternion Rotation { get; set; }
+}
